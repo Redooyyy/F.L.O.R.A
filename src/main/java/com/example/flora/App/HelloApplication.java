f22852ea8d7c1161342bfef15220cb848de930a0
Redooyyy <rwarieal@gmail.com>
2026-04-23 20:02:50 +0600

    chore: disable window resizable and fixed window size for better view

--- src/main/java/com/example/flora/App/HelloApplication.java
+++ src/main/java/com/example/flora/App/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/test/ui/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/Auth/UI/LoginUI.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1102, 588);
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
