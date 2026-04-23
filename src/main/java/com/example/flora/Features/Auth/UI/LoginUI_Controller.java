eb2f991ee4d8445448a8fa61505d169cd4845524
Redooyyy <rwarieal@gmail.com>
2026-04-24 02:01:13 +0600

    feat(auth): Auth UI updated

--- src/main/java/com/example/flora/Features/Auth/UI/LoginUI_Controller.java
+++ src/main/java/com/example/flora/Features/Auth/UI/LoginUI_Controller.java
@@ -3,44 +3,120 @@ package com.example.flora.Features.Auth.UI;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginUI_Controller implements Initializable {
+public class LoginUI_Controller{
     boolean slideToggle = true;
     @FXML
     private AnchorPane slidePane;
     @FXML
+    private ImageView work;
+    @FXML
+    private ImageView robo;
+    @FXML
+    private ImageView collabImg;
+    @FXML
+    private ImageView welcomeTxt;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button signupButton;
+    @FXML
     private Button signRegButton;
     @FXML
-    private ImageView work;
+    private Circle disCir;
+    @FXML
+    private Circle cirU;
+    @FXML
+    private Circle cirD;
+    @FXML
+    private TextField password;
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField rePass;
+    @FXML
+    private TextField pass;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField fullname;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
 
-    }
-@FXML
-void register(ActionEvent event){
-
+    @FXML
+    void register(ActionEvent event){
     if (slideToggle) {
-        signRegButton.setText("SignIn");
-    } else {
-        signRegButton.setText("Register");
+        signRegButton.setText("LOGIN");
+        } else {
+        signRegButton.setText("SIGNUP");
+        }
+    visibilityControl();
+    slider();
     }
-    slideLeft();
-}
-    void slideLeft(){
-        TranslateTransition transition = new TranslateTransition();
-        transition.setNode(slidePane);
-        transition.setDuration(Duration.millis(500));
-        transition.setByX(slideToggle?629:-629);
-        transition.play();
+    void slider(){
+        //bar
+        TranslateTransition regBlockTransition = new TranslateTransition();
+        regBlockTransition.setNode(slidePane);
+        regBlockTransition.setDuration(Duration.millis(500));
+        regBlockTransition.setByX(slideToggle?629:-629);
+        regBlockTransition.play();
+
+        //robo
+        TranslateTransition roboTransition = new TranslateTransition();
+        roboTransition.setNode(robo);
+        roboTransition.setDuration(Duration.millis(500));
+        roboTransition.setByX(slideToggle?-950:950);
+        roboTransition.play();
+
+        //circles
+        TranslateTransition cirTransition1 = new TranslateTransition();
+        cirTransition1.setNode(cirD);
+        cirTransition1.setDuration(Duration.millis(900));
+        cirTransition1.setByX(slideToggle?-268:268);
+        cirTransition1.setByY(slideToggle?35:-35);
+        cirTransition1.play();
+
+        TranslateTransition cirTransition2 = new TranslateTransition();
+        cirTransition2.setNode(cirU);
+        cirTransition2.setDuration(Duration.millis(900));
+        cirTransition2.setByX(slideToggle?85:-85);
+        cirTransition2.setByY(slideToggle?386:-386);
+        cirTransition2.play();
+
+        TranslateTransition cirTransition3 = new TranslateTransition();
+        cirTransition3.setNode(disCir);
+        cirTransition3.setDuration(Duration.millis(900));
+        cirTransition3.setByX(slideToggle?78:-78);
+        cirTransition3.setByY(slideToggle?-499:499);
+        cirTransition3.play();
+
         slideToggle=!slideToggle;
     }
+
+
+    void visibilityControl(){
+       username.setVisible(!slideToggle);
+       password.setVisible(!slideToggle);
+       loginButton.setVisible(!slideToggle);
+       collabImg.setVisible(!slideToggle);
+       welcomeTxt.setVisible(!slideToggle);
+
+       fullname.setVisible(slideToggle);
+       email.setVisible(slideToggle);
+       pass.setVisible(slideToggle);
+       rePass.setVisible(slideToggle);
+       signupButton.setVisible(slideToggle);
+    }
+    @FXML
+    private void signup(ActionEvent event) {}
+    @FXML
+    private void login(ActionEvent event) {}
 }
