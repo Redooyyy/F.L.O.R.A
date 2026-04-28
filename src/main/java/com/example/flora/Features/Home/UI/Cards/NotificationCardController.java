3e14a205db670330374538c6f65155766ec240a6
Redooyyy <rwarieal@gmail.com>
2026-04-29 03:54:16 +0600

    feat(home): delete specific notification logic added

--- src/main/java/com/example/flora/Features/Home/UI/Cards/NotificationCardController.java
+++ src/main/java/com/example/flora/Features/Home/UI/Cards/NotificationCardController.java
@@ -1,12 +1,18 @@
 package com.example.flora.Features.Home.UI.Cards;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class NotificationCardController {
 
     @FXML
     private Label notificationDescription;
+
+    @FXML
+    private AnchorPane card;
 
     @FXML
     private Label notificationTime;
@@ -18,6 +24,15 @@ public class NotificationCardController {
         this.notificationDescription.setText(notificationDescription);
         this.notificationTitle.setText(notificationTitle);
         this.notificationTime.setText(notificationTime);
+    }
+
+    @FXML
+    private void delete(){
+        Parent parent = card.getParent();
+
+        if(parent instanceof Pane pane){
+            pane.getChildren().remove(card);
+        }
     }
 
 }
