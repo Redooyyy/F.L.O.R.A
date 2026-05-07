039079bdbdb5732d677480c5a2adf62ec437ce84
Redooyyy <rwarieal@gmail.com>
2026-05-08 03:00:33 +0600

    feat(task): added constructor for task view model in task UI controller

---
+++ src/main/java/com/example/flora/Features/Task/UI/TaskUI_Controller.java
@@ -0,0 +1,11 @@
+package com.example.flora.Features.Task.UI;
+
+import com.example.flora.Features.Task.ViewModel.TaskViewModel;
+
+public class TaskUI_Controller {
+    private final TaskViewModel taskViewModel;
+
+    public TaskUI_Controller(TaskViewModel taskViewModel){
+        this.taskViewModel = taskViewModel;
+    }
+}
