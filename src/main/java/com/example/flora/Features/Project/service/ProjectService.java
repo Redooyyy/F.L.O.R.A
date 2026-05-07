e9d1035c70bdf48a6102eae8d67f4fe2ba2212ee
Redooyyy <rwarieal@gmail.com>
2026-05-08 02:12:10 +0600

    feat(project): added project service

---
+++ src/main/java/com/example/flora/Features/Project/service/ProjectService.java
@@ -0,0 +1,38 @@
+package com.example.flora.Features.Project.service;
+
+import com.example.flora.Core.Helper.DateAndTime;
+import com.example.flora.Features.Project.model.Project;
+import com.example.flora.Features.Project.repository.ProjectRepository;
+
+import java.util.List;
+
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository){
+        this.projectRepository = projectRepository;
+    }
+
+    public Project createProject(String name, String description, String ownerID){
+        //TODO: must define project ID logic
+        Project project = new Project("121",name,description,ownerID, DateAndTime.now());
+        projectRepository.save(project);
+        return project;
+    }
+
+    public void updateProject(Project project) {
+        projectRepository.update(project);
+    }
+
+    public void deleteProject(String id) {
+        projectRepository.delete(id);
+    }
+
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public List<Project> getProjectsByOwner(String ownerId) {
+        return projectRepository.findByOwnerId(ownerId);
+    }
+}
