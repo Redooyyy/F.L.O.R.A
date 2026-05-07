cb5794d84283ea2c94baedf9fc76c2eadd490d02
Redooyyy <rwarieal@gmail.com>
2026-05-08 00:02:40 +0600

    feat(DI): authview model pass from main function

--- src/main/java/com/example/flora/App/HelloApplication.java
+++ src/main/java/com/example/flora/App/HelloApplication.java
@@ -2,19 +2,26 @@ package com.example.flora.App;
 
 import com.example.flora.Core.Constants.Path;
 import com.example.flora.Core.Constants.WindowConstants;
+import com.example.flora.Core.DI.AppContainer;
+import com.example.flora.Features.Auth.UI.LoginUI_Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, SQLException {
+        AppContainer appContainer = new AppContainer();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(Path.LOGIN));
+
+        fxmlLoader.setControllerFactory(e->new LoginUI_Controller(appContainer.getAuthViewModel()));
+
         Scene scene = new Scene(fxmlLoader.load(),WindowConstants.width,WindowConstants.height);
-        stage.setTitle("Hello!");
+        stage.setTitle("F.L.O.R.A");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
