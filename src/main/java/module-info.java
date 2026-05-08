ef0acff15c5e728ce41a6ba624418feb9c15b2a6
Redooyyy <rwarieal@gmail.com>
2026-05-09 01:29:39 +0600

    chore: remove unwanted file from module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,6 @@ module com.example.flora {
 
     // Only open UI packages (FXML needs reflection)
     opens com.example.flora.Features.Auth.UI to javafx.fxml;
-    opens com.example.flora.Features.test.ui to javafx.fxml;
     opens com.example.flora.Features.Home.UI to javafx.fxml;
     opens com.example.flora.Features.Home.UI.Cards to javafx.fxml;
     opens com.example.flora.Features.Project.UI to javafx.fxml;
