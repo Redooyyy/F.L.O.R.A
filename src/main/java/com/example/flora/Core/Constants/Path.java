994213b108894ee31689f8b4015976f1d4466323
Redooyyy <rwarieal@gmail.com>
2026-05-09 01:24:14 +0600

    feat(task): added task UI to navigation path

--- src/main/java/com/example/flora/Core/Constants/Path.java
+++ src/main/java/com/example/flora/Core/Constants/Path.java
@@ -4,5 +4,6 @@ public class Path {
     public static String LOGIN = "/Auth/UI/LoginUI.fxml";
     public static String HOME = "/Home/UI/HomeUI.fxml";
     public static String PROJECT = "/Project/UI/ProjectUI.fxml";
-    public static  String OVERVIEW = "/Overview/UI/Overview.fxml";
+    public static String OVERVIEW = "/Overview/UI/Overview.fxml";
+    public static String TASK = "/Task/UI/TaskUI.fxml";
 }
