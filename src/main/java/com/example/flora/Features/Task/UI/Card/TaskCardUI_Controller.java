6b9d4b01503094716bdcb2bce73486196db3706a
Redooyyy <rwarieal@gmail.com>
2026-05-08 18:42:04 +0600

    feat(task): added task card UI and controller

---
+++ src/main/java/com/example/flora/Features/Task/UI/Card/TaskCardUI_Controller.java
@@ -0,0 +1,11 @@
+package com.example.flora.Features.Task.UI.Card;
+
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class TaskCardUI_Controller {
+    public Text taskTitle;
+    public Label dueDate;
+    public Label taskStatus;
+    public Text subTitle;
+}
