4da67a7950d5d0f5bc06f4f261df640923e4af52
Redooyyy <rwarieal@gmail.com>
2026-05-08 18:42:57 +0600

    feat(task): added task page initial design and controller and style.css for scrollbar transeparency

--- src/main/java/com/example/flora/Features/Task/UI/TaskUI_Controller.java
+++ src/main/java/com/example/flora/Features/Task/UI/TaskUI_Controller.java
@@ -1,9 +1,14 @@
 package com.example.flora.Features.Task.UI;
 
 import com.example.flora.Features.Task.ViewModel.TaskViewModel;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class TaskUI_Controller {
     private final TaskViewModel taskViewModel;
+    public Label projectNameInViewBox;
+    public VBox projectCardScroll;
+    public VBox TaskCardScroll;
 
     public TaskUI_Controller(TaskViewModel taskViewModel){
         this.taskViewModel = taskViewModel;
