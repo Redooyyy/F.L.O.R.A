80c1a5426d8b60409bb8483bda746b4a82919081
Redooyyy <rwarieal@gmail.com>
2026-05-09 01:24:58 +0600

    chore: added task ui and task ui card to module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,6 +13,8 @@ module com.example.flora {
     opens com.example.flora.Features.Project.UI to javafx.fxml;
     opens com.example.flora.Features.Project.UI.Card to javafx.fxml;
     opens com.example.flora.Features.Overview.UI to javafx.fxml;
+    opens com.example.flora.Features.Task.UI to javafx.fxml;
+    opens com.example.flora.Features.Task.UI.Card to  javafx.fxml;
 
     // Export only what’s needed
     exports com.example.flora.App;
