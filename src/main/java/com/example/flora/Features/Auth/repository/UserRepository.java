fc17a2cece2468a60d56810423becac3f01516e5
Redooyyy <rwarieal@gmail.com>
2026-05-27 23:00:17 +0600

    feat(pop_suggesion): added query for getting matching user name users

--- src/main/java/com/example/flora/Features/Auth/repository/UserRepository.java
+++ src/main/java/com/example/flora/Features/Auth/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.example.flora.Features.Auth.repository;
 
 import com.example.flora.Features.Auth.model.User;
 
+import java.util.List;
+
 public interface UserRepository {
     void saveUser(User user);
     User findByID(Integer id);
     User findByEmail(String email);
     boolean userExist(String email);
+    List<String> searchByUsernameLike(String query);
 }
