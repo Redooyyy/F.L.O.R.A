e01cec4b0f5cfa3d8d8455379d30b0557ec37266
Redooyyy <rwarieal@gmail.com>
2026-05-29 18:34:17 +0600

    feat(notification): passing project id for tracking projects -_-

--- src/main/java/com/example/flora/Features/Home/repository/NotificationRepository.java
+++ src/main/java/com/example/flora/Features/Home/repository/NotificationRepository.java
@@ -1,12 +1,15 @@
 package com.example.flora.Features.Home.repository;
 
 import com.example.flora.Features.Home.model.Notification;
+import com.example.flora.Features.Home.model.NotificationType;
 
 import java.util.List;
 
 public interface NotificationRepository {
 
     void createNotification(int userId, String title, String description);
+
+    public void createNotification(int userId, String title, String description, NotificationType type, String senderName, String projectName, String role, Integer projectId);
 
     List<Notification> getNotifications(int userId);
 
